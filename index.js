2112e3db0e72d350abf92734fe825be1188e59f5
Eyas Ranjous <eyas.ranjous@gmail.com>
2018-02-18 01:10:06 -0600

    es6 code conventions

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+/*!
+ * file-manager-js
+ * Entry Point
+ * Copyright(c) 2018 Eyas Ranjous <eyas.ranjous@gmail.com>
+ * MIT Licensed
+ */
+
 'use strict';
 
-const fs          = require('fs'),
-      path        = require('path'), 
-      FileManager = require('./lib/fileManager');
+const fs          = require('fs');
+const path        = require('path'); 
+const FileManager = require('./lib/fileManager');
 
 module.exports = { create: () => new FileManager(fs, path) };
