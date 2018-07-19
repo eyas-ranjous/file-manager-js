5d21a7c23fca43e261b5f848705be5bf394c0859
Eyas Ranjous <eyas.ranjous@gmail.com>
2018-07-19 13:10:47 -0500

    refactor

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
  * @license MIT
  */
 
-const fs = require('fs');
 const { join } = require('path');
 const stat = require('./lib/stat');
 const info = require('./lib/info');
@@ -21,15 +20,15 @@ const removeFile = require('./lib/removeFile');
 // fileManager api
 module.exports = {
   join,
-  stat: stat(fs.stat),
-  info: info(fs.readdir, fs.stat, join),
-  list: list(fs.readdir, fs.stat, join),
-  listDeep: listDeep(fs.readdir, fs.stat, join),
-  exists: exists(fs.stat),
-  rename: rename(fs.rename),
-  createDir: createDir(fs.mkdir, fs.stat),
-  removeDir: removeDir(fs.rmdir, fs.unlink, fs.readdir, fs.stat, join),
-  createFile: createFile(fs.open, fs.mkdir, fs.stat),
-  readFile: readFile(fs.readFile),
-  removeFile: removeFile(fs.unlink)
+  stat,
+  info,
+  list,
+  listDeep,
+  exists,
+  rename,
+  createDir,
+  removeDir,
+  createFile,
+  readFile,
+  removeFile
 };
